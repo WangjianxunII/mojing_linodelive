9dc3399df84b6073da54a300cef172466b74c93d
wangjianxunII <jianxun.wang@mojing.com>
2022-05-09 17:50:24 +0800

    代码

--- clean_item.py
+++ clean_item.py
@@ -6,7 +6,7 @@ class CLEAN_DATA:
 
     def get_content(self):
         # with open('wangtest.txt','r') as f:
-        with open('/data/var/log/catappitems_test9_log_2022_05_09_26_rproxy.log','r') as f:
+        with open('/data/var/log/catappitems_tes11_log_2022_05_09_26_rproxy.log','r') as f:
             all_content = f.read()
             datalist = re.findall('seed:(.*?),items:', all_content)
             print "去重前 抓取数据：",len(datalist)*10
