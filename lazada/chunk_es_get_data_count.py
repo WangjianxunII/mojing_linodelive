6a58fa6d0bfd5544b7cfbb376b0b5b04f7be64c4
wangjianxunII <jianxun.wang@mojing.com>
2022-06-09 17:03:55 +0800

    更新查询去重后数据数量

---
+++ lazada/chunk_es_get_data_count.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from elasticsearch import Elasticsearch
+
+es = 'http://10.19.12.67:9200'
+ess = Elasticsearch(es)
+index = 'lazada_sg_2022_06_08'
+body = {"query": {"match_all": {}}}
+# body={
+#     "query":{
+#         'bool':{"must":[{ "term": {
+#          u'__cat1_id': u'275',
+#         }},
+#          { "term": {
+#          u'__sold' : 0
+#         }}
+#                        ]}
+
+#     }
+# }
+res = ess.search(index=index, body=body, scroll='5m', size=10000)
+hits = res['hits']['total']
+print hits
+# wf =open('/share/home/maweiwei/wmatool/lazada/lazada_sg_4.txt','w')
+# for m in res['hits']['hits']:
+#     x = m['_source']
+#     sold = x['__sold']
+#     if int(sold) ==0:
+#         continue
+#     data = x['__item_id']
+#     wf.write(data +'\n')
+
+# wf.close()
