f893705ae72c5597ac603a55bdb18fb2c9b4da99
wangjianxunII <jianxun.wang@mojing.com>
2022-06-20 14:16:22 +0800

    新增data统计

--- lazada/clean_item.py
+++ lazada/clean_item.py
@@ -3,7 +3,7 @@ import json
 import random
 import re
 import time
-
+from elasticsearch import Elasticsearch
 
 class CLEAN_DATA:
     def __init__(self):
@@ -74,10 +74,125 @@ class CLEAN_DATA:
                 # print self.data_list
         print "去重前 抓取数据：", d_count
         print "去重后 抓取数据：",len(self.data_list)
+    def get_list_content4(self):
+
+        sold_count_lsit = []
+        # with open('wangtest.txt','r') as f:
+        for p in range(1,10):
+            try:
+                with open('/data/lazada_ph/lazada_ph__2022_05_28_%s_rproxy.txt'%p, 'r') as f:
+                # with open('./%s.txt'%p, 'r') as f:
+                    for line in f:
+                        sold_count_l = re.findall('__sold": "(.*?)", "', line)
+                        item_id_l = re.findall('__item_id": "(.*?)", "', line)
+                        # print 'item_id:', item_id_l
+                        item = {}
+                        if item_id_l:
+                            if item_id_l[0]:
+                                item['item_id'] = item_id_l[0]
+                            else:
+                                item['item_id'] = ''
+                        else:
+                            print line
+                            item['item_id'] = ''
+                            # break
+                        if sold_count_l:
+                            if sold_count_l[0]:
+                                item['sold_count'] = sold_count_l[0]
+                            else:
+                                item['sold_count'] = ''
+                        else:
+                            item['sold_count'] = ''
+                        if item['item_id'] not in sold_count_lsit and item['sold_count']:
+                            sold_count_lsit.append(item['item_id'])
+
+                # print "有销量的数据：", len(sold_count_lsit)
+
+            except:
+                pass
+        print "有销量的数据：", len(sold_count_lsit)
+
+    def get_test(self):
+        sold_count_lsit = []
+        with open('/data/lazada_ph/lazada_ph__2022_05_28_%s_rproxy.txt' % 1, 'r') as f:
+            # with open('./%s.txt'%p, 'r') as f:
+            for line in f:
+                sold_count_l = re.findall('__sold": "(.*?)", "', line)
+                item_id_l = re.findall('__item_id": "(.*?)", "', line)
+                # print 'item_id:', item_id_l
+                item = {}
+                if item_id_l:
+                    if item_id_l[0]:
+                        item['item_id'] = item_id_l[0]
+                    else:
+                        item['item_id'] = ''
+                else:
+                    print line
+                    item['item_id'] = ''
+                    break
+                if sold_count_l:
+                    if sold_count_l[0]:
+                        item['sold_count'] = sold_count_l[0]
+                    else:
+                        item['sold_count'] = ''
+                else:
+                    item['sold_count'] = ''
+                if item['item_id'] not in sold_count_lsit and item['sold_count']:
+                    sold_count_lsit.append(item['item_id'])
+
+        print "有销量的数据：", len(sold_count_lsit)
+
+    def get_test_2(self):
+        es = 'http://10.19.12.67:9200'
+        ess = Elasticsearch(es)
+        item_list = []
+        index = 'lazada_sg_2022_05_26'
+        # 根据索引获取es中的数据
+        bulk_num = 5000
+        body = {"query": {"match_all": {}}}
+        res = ess.search(index=index, body=body, scroll='5m', size=bulk_num, request_timeout=60)
+        hits = res['hits']['hits']
+        total = res['hits']['total']['value']
+        scroll_id = res['_scroll_id']
+        f_num = total / bulk_num + 1
+        sold_count = 0
+        for i in range(0, f_num):
+            query_scroll = ess.scroll(scroll_id=scroll_id, scroll='5m', request_timeout=60)['hits']['hits']
+            if i == 0:
+                query_scroll += hits
+
+            for q in query_scroll:
+                if q.get('__sold') > 0:
+                    if q.get('__item_id') not in item_list:
+                        sold_count += 1
+        print "有销量的数据：", sold_count
+        print "有销量的数据itemlist：", len(item_list)
+    def get_content5(self):
+        with open('/data/lazada_detail/lazada_detail_sg_2022_06_13_1_rproxy.txt') as f:
+        # with open('./test_') as f:
+            d_count = 0
+            sold_count = 0
+            no_sold_count = 0
+            for line in f:
+                d_count += 1
+                datalist = re.findall('__sold": "(.*?)", "guideApp":', line)
+                # print datalist
+                for i in datalist:
+                    if i:
+                        sold_count += 1
+                    else:
+                        no_sold_count += 1
+            print "有销量的数据：", sold_count
+            print "没有销量的数据：", no_sold_count
+            print "总商品数：", d_count
 clean_data = CLEAN_DATA()
 # clean_data.get_content()
 # clean_data.get_content2()
-clean_data.get_content3()
+# clean_data.get_content3()
+# clean_data.get_list_content4()
+# clean_data.get_test()
+clean_data.get_test_2()
+# clean_data.get_content5()
 # clean_data.get_Ua()
 
 #worker starting
---
+++ lazada/statistics_lazada_es.py
@@ -0,0 +1,40 @@
+# coding:utf-8
+import pandas as pd
+from elasticsearch import Elasticsearch
+
+es = 'http://10.19.12.67:9200'
+ess = Elasticsearch(es)
+# 'lazada_ph_2022_05_28','lazada_th_2022_05_28','lazada_vn_2022_05_27',
+indexs = ['lazada_id_2022_05_26','lazada_my_2022_05_26','lazada_sg_2022_05_26']
+
+
+# body={"query":{"match_all":{}}}
+def get_sub(index):
+    body = {"query": {"match_all": {}}}
+
+    body1 = {
+        "query": {
+            'bool': {"must": [{"term": {
+                u'__sold': 0
+            }}
+            ]}
+
+        }
+    }
+    res = ess.search(index=index, body=body, scroll='5m', size=10000)
+    hits = res['hits']['total']
+    # print(hits)
+
+    res1 = ess.search(index=index, body=body1, scroll='5m', size=10000)
+    hits1 = res1['hits']['total']
+    # print(hits1)
+    print "有销量的数据：",index, (hits.get('value') - hits1.get('value'))
+    print "没有销量的数据：",index, hits1.get('value')
+    print "总商品数：",index, hits.get('value')
+    print "*********"*5
+
+def get_sub_item(index):
+    pass
+
+for i in indexs:
+    get_sub(i)
